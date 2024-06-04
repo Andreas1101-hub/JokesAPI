73237e1063a682e3dce71c7b56d5ffb2a3b05fc2
Andreas Kwong <andreaskwong@gmail.com>
2024-06-04 12:56:09 +0100

    Initial Commit

---
+++ JokesAPI/JokesAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAPIApp.swift
+//  JokesAPI
+//
+//  Created by Andreas Kwong on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct JokesAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
